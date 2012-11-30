be6930d6994585f910a22eeff96f73484cb921f0
Bryan Hunt <bhunt@mac.com>
2012-11-29 22:35:12 -0600

    Cleaned up dependencies

--- bundles/org.eclipselabs.eunit.junit.utils/src/org/eclipselabs/eunit/junit/utils/bundle/Activator.java
+++ bundles/org.eclipselabs.eunit.junit.utils/src/org/eclipselabs/eunit/junit/utils/bundle/Activator.java
@@ -11,10 +11,8 @@
 
 package org.eclipselabs.eunit.junit.utils.bundle;
 
-import org.eclipselabs.emf.ext.ResourceSetFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author bhunt
@@ -27,28 +25,17 @@ public class Activator implements BundleActivator
 		return bundleContext;
 	}
 
-	public static ResourceSetFactory getResourceSetFactory()
-	{
-		return resourceSetFactoryTracker.getService();
-	}
-
 	@Override
 	public void start(BundleContext context) throws Exception
 	{
 		bundleContext = context;
-		resourceSetFactoryTracker = new ServiceTracker<ResourceSetFactory, ResourceSetFactory>(context, ResourceSetFactory.class, null);
-		resourceSetFactoryTracker.open();
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception
 	{
-		if (resourceSetFactoryTracker != null)
-			resourceSetFactoryTracker.close();
-
 		bundleContext = null;
 	}
 
 	private static BundleContext bundleContext;
-	private static ServiceTracker<ResourceSetFactory, ResourceSetFactory> resourceSetFactoryTracker;
 }
